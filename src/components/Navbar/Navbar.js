a4817b2fccb66554514f8ee155e5baff1ad0479b
alanbecc <klog66@hotmail.com>
2025-02-19 13:42:57 -0500

    First Commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import * as S from './Navbar.styles';
+
+const Navbar = () => {
+  return (
+    <S.ContainerNavbar>
+        Nuestro Primer Container Navbar
+    </S.ContainerNavbar>
+  )
+}
+
+export default Navbar;
