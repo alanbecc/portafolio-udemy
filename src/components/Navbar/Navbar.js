65b4450df486c80653824bcb2d0c679edc8f1820
alanbecc <klog66@hotmail.com>
2025-02-19 15:05:38 -0500

    feat: add burguer menu mobile and desktop

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,34 @@
-import React from 'react'
+import { useState} from 'react'
 import * as S from './Navbar.styles';
 
 const Navbar = () => {
+
+    const [showMenuBurguer, setShowMenuBurguer] = useState(false);
+
+    
+
   return (
     <S.ContainerNavbar>
-        Nuestro Primer Container Navbar
+      <S.ContainerLogo>
+        <S.ImageLogo src="/img/Logo Camilo.png" alt='Logo Camilo G' />
+        <div>
+            <h1>Camilo G</h1>
+            <h2>Frontend Developer</h2>
+        </div>
+        </S.ContainerLogo>
+
+        <S.ContainerBurguer onClick={() => setShowMenuBurguer( !showMenuBurguer)}>
+            <S.BurguerMenu></S.BurguerMenu>
+            <S.BurguerMenu></S.BurguerMenu>
+            <S.BurguerMenu></S.BurguerMenu>
+        </S.ContainerBurguer>
+
+        <S.ContainerItems showMenuBurguer={showMenuBurguer}>
+            <p>About Me</p>
+            <p>Projects</p>
+            <S.Button>Contact Me  🤙🏼</S.Button>
+        </S.ContainerItems>
+      
     </S.ContainerNavbar>
   )
 }
